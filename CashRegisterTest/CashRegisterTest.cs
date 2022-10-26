b56cab513188a939231f253285bf72d5dd4cb211
xma12 <xma12@slb.com>
2022-10-26 16:08:40 +0800

    test(Question1): add printer.print call test use mock printer

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,6 +1,7 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
+	using Moq;
 	using Xunit;
 
 	public class CashRegisterTest
@@ -16,6 +17,20 @@ namespace CashRegisterTest
 			cashRegister.Process(purchase);
 			//then
 			Assert.True(printer.HasPrinted);
+			//verify that cashRegister.process will trigger print
+		}
+
+		[Fact]
+		public void Should_print_called_when_run_process_given_spy_print()
+		{
+			//given
+			var spyPrinter = new Mock<Printer>();
+			var cashRegister = new CashRegister(spyPrinter.Object);
+			var purchase = new Purchase();
+			//when
+			cashRegister.Process(purchase);
+            //then
+			spyPrinter.Verify(_ => _.Print(It.IsAny<string>()));
 			//verify that cashRegister.process will trigger print
 		}
 	}
